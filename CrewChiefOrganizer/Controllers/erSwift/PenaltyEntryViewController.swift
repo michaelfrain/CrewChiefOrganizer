46e299469dd9fa21e5ec10dc6a2732f66e161b5d
Michael Frain <michaelfrain@Michaels-iMac.local>
2016-07-03 15:21:35 -0700

    Constraints plus new VC

--- CrewChiefOrganizer/Controllers/erSwift/PenaltyEntryViewController.swift
+++ CrewChiefOrganizer/Controllers/erSwift/PenaltyEntryViewController.swift
@@ -1,32 +1,35 @@
 //
-//  DatePickerViewController.swift
+//  PenaltyEntryViewController.swift
 //  CrewChiefOrganizer
 //
-//  Created by Michael Frain on 7/2/16.
+//  Created by Michael Frain on 7/3/16.
 //  Copyright © 2016 Michael Frain. All rights reserved.
 //
 
 import UIKit
 
-class DatePickerViewController: UIViewController {
-    @IBOutlet var datePicker: UIDatePicker!
+class PenaltyEntryViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
     }
-    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
     
+
+    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: AnyObject?) {
-        if segue.identifier == "UnwindFromDatePickerSegue" {
-            let gameSetupController = segue.destinationViewController as! GameSetupViewController
-            gameSetupController.gameDate = datePicker.date
-        }
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
     }
+    */
+
 }
